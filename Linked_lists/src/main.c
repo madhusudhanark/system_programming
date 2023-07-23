d28bd023c6a8b90a0ba7ecb2f647c0f975491dbc
Madhusudhana Kumar <madhuit015@gmail.com>
2023-07-23 08:00:05 +0530

    updated linked list

--- Linked_lists/src/main.c
+++ Linked_lists/src/main.c
@@ -3,38 +3,53 @@
 
 struct Node
 {
-    int x;
-    struct Node *next;
+  int          x;
+  struct Node *next;
 };
 
-int addElements(int number, struct Node *head)
+struct Node *addElements(int number, struct Node *head)
 {
-    struct Node *current;
-    current = head;
-    for (int i = 0; i < number; i++)
-    {
-        current = malloc(sizeof(struct Node));
-        if (i == number - 1)
-        {
-            current->next = NULL;
-        }
-        else
-        {
+  struct Node *current;
 
-            current = current->next;
-        }
-    }
+  if (number <= 0)
+  {
+    return head;
+  }
+
+  if (head == NULL)
+  {
+    head = malloc(sizeof(struct Node));
+    printf("inside func head %p\n", head);
+  }
+
+  current = head;
+  for (int i = 0; i < number - 1; i++)
+  {
+    current->next = malloc(sizeof(struct Node));
+    printf("inside func current %p\n", current->next);
+
+    current = current->next;
+  }
+
+  current->next = NULL;
+
+  printf("inside func head %p\n", head);
+
+  return head;
 }
 
 int main()
 {
-    struct Node *head;
-    struct Node *curr;
+  struct Node *head = NULL;
+  struct Node *curr;
 
-    addElements(10, head);
+  head = addElements(10, head);
 
-    for (curr = head; curr != NULL; curr = curr->next)
-    {
-        printf("%p has %d\n", curr, curr->x);
-    }
+  printf("outside func head %p\n", head);
+  printf("outside func head->next %p\n", head->next);
+
+  for (curr = head; curr != NULL; curr = curr->next)
+  {
+    printf("%p has %d\n", curr);
+  }
 }
