902021688ad16b0ad689050594e3f535cdd3a774
Madhusudhana Kumar <madhuit015@gmail.com>
2023-08-07 00:34:11 +0530

    tested server client program

--- Threads/server.c
+++ Threads/server.c
@@ -9,12 +9,21 @@
 
 int main(int argc, char const *argv[])
 {
-  int socket_fd;
+  int                socket_fd;
+  int                opt = 1;
+  struct sockaddr_in address;
 
   if (socket(AF_INET, SOCK_STREAM, 0) < 0)
   {
     perror("error in socket creation");
   };
 
-  if (getsockopt())
+  if (gsetsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+  {
+    perror("setsokcet");
+    exit(EXIT_FAILURE);
+  }
+  address.sin_family      = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port        = htons(PORT);
 }
